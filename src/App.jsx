fdde89e503d1d5beea7372e656165230c15a8824
marco bruno <marco2000bruno@gmail.com>
2023-10-13 17:25:50 +0200

    esercizio29

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import AlertClock from './alertClock'
-
+import { Counter } from './counter'
 
 
 function App() {
-  function handleClock() {
-    
-    const clock = new Date()
-
-      alert(`it is the time ${clock.toLocaleTimeString()}`)
-
-  }
-
   return(
-    <>
-      <AlertClock event = {handleClock} />
-    </>
+     <>
+      <Counter />
+     </>
   )
 }
-
-
 export default App
--- src/alertClock.jsx
+++
@@ -1,9 +0,0 @@
-function AlertClock({event}) {
-
-    return(
-        <button onClick={event}>time!</button>
-      )
-    
-  }
-  export default AlertClock
-  
