982d06c49ebf4164007cfcfaf3c3c9fdf7b37383
marco bruno <marco2000bruno@gmail.com>
2023-10-14 14:20:37 +0200

    esercizio35

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Counter } from './counter'
+import { Clock } from './clock'
 
 
 function App() {
   return(
      <>
       <Counter  />
+      <Clock />
      </>
   )
 }
