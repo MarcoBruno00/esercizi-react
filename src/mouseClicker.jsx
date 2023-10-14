bdfa4900a65186bc270e6c5096e5699b2077c7dd
marco bruno <marco2000bruno@gmail.com>
2023-10-14 19:45:40 +0200

    esercizio37

--- src/mouseClicker.jsx
+++ src/mouseClicker.jsx
@@ -1,11 +1,14 @@
 export function MouseClicker({name}) {
     function handleClick(event) {
-        console.log(event.target.name);
+        console.log(event.currentTarget.name);
     }
 
 
 
     return(
-        <button name={name} onClick={handleClick}>click me!</button>
+        <button name={name} onClick={handleClick}>
+            <img src="" alt="" width={24} height={24} />
+            click me!
+            </button>
     )
 }
