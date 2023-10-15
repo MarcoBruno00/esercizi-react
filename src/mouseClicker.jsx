7c5c8ade61924cca2ee6085a994b4c994710b0e1
marco bruno <marco2000bruno@gmail.com>
2023-10-15 16:37:32 +0200

    esercizio38

--- src/mouseClicker.jsx
+++ src/mouseClicker.jsx
@@ -1,13 +1,22 @@
+import faviconImage from "./assets/favicon-16x16.png";
+
+
 export function MouseClicker({name}) {
     function handleClick(event) {
         console.log(event.currentTarget.name);
     }
 
+     function handleImage  (event) {
+        console.log(event.currentTarget.src);
+    }
+
+
+       //vengono eseguiti insieme a causa dell'effetto bubble di html dato che l'immagine si trova all'interno del bottone
 
 
     return(
         <button name={name} onClick={handleClick}>
-            <img src="" alt="" width={24} height={24} />
+            <img src={faviconImage} alt="" width={24} height={24} onClick={handleImage}/>
             click me!
             </button>
     )
