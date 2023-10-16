c9c080aa857fd663b73bc2e4f84e19bced53ac61
marco bruno <marco2000bruno@gmail.com>
2023-10-16 14:18:37 +0200

    esercizio43

--- src/login.jsx
+++ src/login.jsx
@@ -1,43 +1,40 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+export function Login({ onLogin }) {
+  const [data, setData] = useState({
+    username: '',
+    password: '',
+    remember: false
+  });
+
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+
+    setData(data => {
+      return {
+        ...data, 
+        [name]: value
+      }
+    });
+  }
 
 
-export function Login({onLogin}) {
-    const [data , setData] = useState({
-        username:'',
-        password:'',
-        remember: false
-    })
+  const isButtonDisabled = data.username === '' || data.password === '';
 
-    function handleChangeImput(event) {
-        const name = event.target.name
-        const value = event.target.type === "checkbox" ? event.target.checked : event.target.value
+  const handleButton = (event) => {
+    event.preventDafault()
+    onLogin(data);
+  };
 
-        setData(data => {
-            return{
-                ...data,
-                [name]: value
-            }
-        })
-
-        
-    }
-    
-            const isButtonDisalbled = data.username === '' || data.password === ''
-    
-    
-            const handleButton = (event) => {
-                event.preventDefault()
-                onLogin(data)
-            }
-
-    return(
-        <>
-            <form>
-                <input type="text"  name = "username" value={data.username} onChange={handleChangeImput} />
-                <input type="password" name = "password" value={data.password} onChange={handleChangeImput} />
-                <input type="checkbox" name = "remember"checked={data.remember} onChange={handleChangeImput} />
-                <button onClick={handleButton} disabled={isButtonDisalbled}>login</button>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <form>
+        <input type="text" name="username" value={data.username} onChange={handleInputChange} />
+        <input type="password" name="password" value={data.password} onChange={handleInputChange} />
+        <input type="checkbox" name="remember" checked={data.remember} onChange={handleInputChange} />
+        <button onClick={handleButton} disabled={isButtonDisabled}>Login</button>
+      </form>
+    </>
+  )
 }
